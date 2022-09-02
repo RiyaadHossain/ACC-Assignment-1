4e8d5537bea77476f5d32e2962fca8a9594d6882
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-09-02 09:43:41 +0600

    post user successfully added

--- controller/userController.js
+++ controller/userController.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
+let data = fs.readFileSync("data.json")
+let parsedData = JSON.parse(data)
 
 // 1. Get Random User________________________
 module.exports.randomUser = (req, res) => {
 
     const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-    const data = fs.readFileSync("data.json")
 
     if (data) {
-        const parsedData = JSON.parse(data)
         const randomNum = random(1, parsedData.length)
         const randomUser = parsedData.find(user => user.Id == randomNum)
         res.status(200).json({ data: randomUser })
     } else {
-        res.status(500).json("Internal Server Error!")
+        res.status(500).json({ error: "Internal Server Error!" })
 
     }
 
@@ -21,19 +21,36 @@ module.exports.randomUser = (req, res) => {
 // 2. Get All Users________________________
 module.exports.allUser = (req, res) => {
 
-    const {limit} = req.query
-    const data = fs.readFileSync("data.json")
+    const { limit } = req.query
 
     if (data) {
-        const parsedData = JSON.parse(data)
-        const selectedData = parsedData.splice(0, limit)
+        const selectedData = parsedData.splice(0, limit ? limit : Infinity) // * There is a bug using splice, fix it --- splice will removed the item from the array
         res.status(200).json({ data: selectedData })
+    } else {
+        res.status(500).json({ error: "Internal Server Error" })
     }
 
 }
 
 // 3. Save a User________________________
 module.exports.saveUser = (req, res) => {
+
+    const { Id, gender, name, contact, address, photoUrl } = req.body
+    if (Id && gender && name && contact && address && photoUrl) {
+
+        const idExist = parsedData.find(user => user.Id == Number(Id))
+        if (idExist) {
+            res.status(403).json({ error: "Id is already Exist" })
+        } else if (typeof Number(Id) === "number" && Number(Id) > 0) {
+            parsedData.push({ Id, gender, name, contact, address, photoUrl })
+            res.status(201).json({ message: "Your data has been saved successfully." })
+        } else {
+            res.status(403).json({ error: "The Id you privided isn't correct!" })
+        }
+
+    } else {
+        res.status(403).json({ error: "You must provide Id, gender, name, contact, address, photoUrl properties" })
+    }
 
 }
 
