912d9df4c5f0a3c86a14a4efeddd56a6edca6809
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-09-02 20:36:26 +0600

    vercel.json file added

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const userRoute = require("./router/userRoute")
 
 const app = express()
 
+// Middleware
 app.use(express.json())
 app.use("/user", userRoute)
 
+// Health Check
 app.get("/", (req, res) => {
     res.send("Hello World!")
 })
 
+// Listen to PORT
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`)
 })
