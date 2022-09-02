f994396f732450b07e4e4cd26af8a85491b25b69
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-09-02 11:32:52 +0600

    update single user route added

--- controller/userController.js
+++ controller/userController.js
@@ -9,7 +9,7 @@ module.exports.randomUser = (req, res) => {
 
     if (data) {
         const randomNum = random(1, parsedData.length)
-        const randomUser = parsedData.find(user => user.Id == randomNum)
+        const randomUser = parsedData.find(user => user.Id == Number(randomNum))
         res.status(200).json({ data: randomUser })
     } else {
         res.status(500).json({ error: "Internal Server Error!" })
@@ -24,7 +24,8 @@ module.exports.allUser = (req, res) => {
     const { limit } = req.query
 
     if (data) {
-        const selectedData = parsedData.splice(0, limit ? limit : Infinity) // * There is a bug using splice, fix it --- splice will removed the item from the array
+        // const selectedData = parsedData.splice(0, limit ? limit : Infinity) // * There is a bug using splice, fix it --- splice will removed the item from the array
+        const selectedData = parsedData
         res.status(200).json({ data: selectedData })
     } else {
         res.status(500).json({ error: "Internal Server Error" })
@@ -41,7 +42,7 @@ module.exports.saveUser = (req, res) => {
         const idExist = parsedData.find(user => user.Id == Number(Id))
         if (idExist) {
             res.status(403).json({ error: "Id is already Exist" })
-        } else if (typeof Number(Id) === "number" && Number(Id) > 0) {
+        } else if (typeof Number(Id) == "number" && Number(Id) > 0) {
             parsedData.push({ Id, gender, name, contact, address, photoUrl })
             fs.writeFileSync("data.json", JSON.stringify(parsedData))
             res.status(201).json({ message: "Your data has been saved successfully." })
@@ -58,6 +59,24 @@ module.exports.saveUser = (req, res) => {
 // 4. Update a User________________________
 module.exports.updateUser = (req, res) => {
 
+    const { Id, gender, name, contact, address, photoUrl } = req.body
+    if (!Id || !gender || !name || !contact || !address || !photoUrl) {
+        return res.status(403).json({ error: "Please provide Id, gender, name, contact, address, photoUrl property." })
+    }
+
+    const updatedUser = { Id, gender, name, contact, address, photoUrl }
+    const userExist = parsedData.find(user => user.Id == Number(Id))
+    if (!userExist) {
+        console.log(userExist)
+        res.status(403).json({ error: "User data not found" })
+    } else if (updatedUser) {
+        parsedData = parsedData.map(user => user.Id != Number(Id) ? user : updatedUser)
+        fs.writeFileSync("data.json", JSON.stringify(parsedData))
+        res.status(201).json({ message: "User data updated successfully" })
+    } else {
+        res.status(500).json({ error: "Internal Server Error" })
+    }
+
 }
 
 // 5. Update Random Users________________________
@@ -69,9 +88,12 @@ module.exports.updateRandomUsers = (req, res) => {
 module.exports.deleteUser = (req, res) => {
     const { Id } = req.body
     const selectedUser = parsedData.filter(user => user.Id !== Number(Id))
-    console.log(selectedUser);
-    if (!typeof Number(Id) === "number") {
-        res.status(403).json({ error: "Please provide the correct Id" })
+
+    if (isNaN(Number(Id)) || !Id) {
+        return res.status(403).json({ error: "Please provide the correct Id" })
+    }
+    else if (parsedData.length == selectedUser.length) {
+        res.status(403).json({ error: "User not found! Please type the correct Id." })
     }
     else if (selectedUser) {
         fs.writeFileSync("data.json", JSON.stringify(selectedUser))
