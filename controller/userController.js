637ca2e31d1f18d60b51e20ed999cade6c11c72a
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-09-02 16:00:53 +0600

    multiple user data update complete

--- controller/userController.js
+++ controller/userController.js
@@ -23,9 +23,9 @@ module.exports.allUser = (req, res) => {
 
     const { limit } = req.query
 
-    if (data) {
-        // const selectedData = parsedData.splice(0, limit ? limit : Infinity) // * There is a bug using splice, fix it --- splice will removed the item from the array
-        const selectedData = parsedData
+    if (parsedData) {
+        const selectedData = parsedData.slice(0, limit ? limit : Infinity)
+        console.log(selectedData);
         res.status(200).json({ data: selectedData })
     } else {
         res.status(500).json({ error: "Internal Server Error" })
@@ -82,6 +82,30 @@ module.exports.updateUser = (req, res) => {
 // 5. Update Random Users________________________
 module.exports.updateRandomUsers = (req, res) => {
 
+    const { Id, gender, name, contact, address, photoUrl } = req.body
+    const updatedUser = { Id, gender, name, contact, address, photoUrl }
+
+    if (!Id || !gender || !name || !contact || !address || !photoUrl) {
+        return res.status(403).json({ error: "Please provide Id, gender, name, contact, address, photoUrl property." })
+    }
+
+    Id.forEach(id => {
+        if (isNaN(id) || !id) {
+            return res.status(403).json({ error: "Please provide valid data" })
+        }
+    })
+
+    parsedData.forEach(user => {
+        Id.filter(id => user.Id == id ? updateUser(user) : null)
+    });
+
+    function updateUser(user) {
+        parsedData = parsedData.map(data => data.Id == user.Id ? { ...updatedUser, Id: user.Id } : data)
+    }
+
+    fs.writeFileSync("data.json", JSON.stringify(parsedData))
+    res.status(201).json({ message: "Users data updated successfully" })
+
 }
 
 // 6. Delete a User________________________
@@ -90,7 +114,7 @@ module.exports.deleteUser = (req, res) => {
     const selectedUser = parsedData.filter(user => user.Id !== Number(Id))
 
     if (isNaN(Number(Id)) || !Id) {
-        return res.status(403).json({ error: "Please provide the correct Id" })
+        res.status(403).json({ error: "Please provide the correct Id" })
     }
     else if (parsedData.length == selectedUser.length) {
         res.status(403).json({ error: "User not found! Please type the correct Id." })
